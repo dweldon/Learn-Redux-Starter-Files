bb5052e3537159f32e1d6cb7d3bce716635b1837
David Weldon <dweldon@gmail.com>
2016-05-18 13:06:24 -0700

    Add basic components.

---
+++ learn-redux/client/components/PhotoGrid.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class PhotoGrid extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="photo-grid">photo-grid
+      </div>
+    );
+  }
+}
---
+++ learn-redux/client/components/Single.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Single extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="single-photo">single
+      </div>
+    );
+  }
+}
