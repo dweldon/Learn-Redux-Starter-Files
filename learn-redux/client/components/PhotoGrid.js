abf8743826af2913dc18f41d1dc5e434e92d47d1
David Weldon <dweldon@gmail.com>
2016-05-19 11:08:50 -0700

    Add photos.

--- learn-redux/client/components/PhotoGrid.js
+++ learn-redux/client/components/PhotoGrid.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Photo from './Photo';
 
 export default class PhotoGrid extends React.Component {
   constructor(props) {
@@ -7,7 +8,10 @@ export default class PhotoGrid extends React.Component {
 
   render() {
     return (
-      <div className="photo-grid">photo-grid
+      <div className="photo-grid">
+        {this.props.posts.map((post, i) =>
+          <Photo {...this.props} key={i} i={i} post={post}/>
+        )}
       </div>
     );
   }
