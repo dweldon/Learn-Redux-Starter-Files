208be6aec168eede78b1add736d9bd7cb0bb01ab
David Weldon <dweldon@gmail.com>
2016-05-20 12:01:22 -0700

    Display a single photo.

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 export default class Single extends React.Component {
   constructor(props) {
@@ -6,8 +8,15 @@ export default class Single extends React.Component {
   }
 
   render() {
+    const i = this.props.posts.findIndex((post) =>
+      post.code === this.props.params.postId
+    );
+    const post = this.props.posts[i];
+    console.log(post);
     return (
-      <div className="single-photo">single
+      <div className="single-photo">
+        <Photo i={i} post={post} {...this.props} />
+        <Comments />
       </div>
     );
   }
